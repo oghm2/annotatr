f9778be0fe55b050bf842165a54d3832e1dab0ca
Jeff Snyder <jeff-github@caffeinated.me.uk>
2013-11-03 14:12:28 +0000

    selection tweaks

--- html/js/tools/select.js
+++ html/js/tools/select.js
@@ -1,5 +1,23 @@
 /*global $:false, jQuery:false */
 
+function select_item(item) {
+    if (item.closed)
+        item.fillColor = new paper.Color(0,0,1,0.2);
+}
+
+function deselect_item(item) {
+    if (item.closed)
+        item.fillColor = new paper.Color(0,0,0,0);
+}
+
+function highlight_item(item) {
+    item.selected = true;
+}
+
+function dehighlight_item(item) {
+    item.selected = false;
+}
+
 var select = {
     loaded : function() {},
     selected: function() {
@@ -9,24 +27,25 @@ var select = {
         console.log("!select");
     },
     click: function() {
+        if(this.state.selected_item)
+            deselect_item(this.state.selected_item);
         this.state.selected_item = this.state.highlighted_item;
+        if(this.state.selected_item)
+            select_item(this.state.selected_item);
     },
     mousemove: function(event) {
         var new_highlighted_item = undefined;
         var hr = paper.project.hitTest(event.point);
-
         if (hr) {
             new_highlighted_item = hr.item;
-        } else {
-            new_highlighted_item = this.state.selected_item;
         }
 
         if (this.state.highlighted_item != new_highlighted_item) {
             if (this.state.highlighted_item)
-                this.state.highlighted_item.selected = false;
+                dehighlight_item(this.state.highlighted_item)
             this.state.highlighted_item = new_highlighted_item;
             if (this.state.highlighted_item)
-                this.state.highlighted_item.selected = true;
+                highlight_item(this.state.highlighted_item)
         }
     },
     state: {
