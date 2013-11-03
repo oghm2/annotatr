c648c36d21cde4dc1920e685162cd413a9305a0d
Jeff Snyder <jeff-github@caffeinated.me.uk>
2013-11-03 13:18:30 +0000

    Add select tool skeleton

---
+++ html/js/tools/select.js
@@ -0,0 +1,11 @@
+/*global $:false, jQuery:false */
+
+var select = {
+    loaded : function() {},
+    selected: function() {
+            console.log("select!");
+        }
+    };
+
+tools.select = select;
+
