0ffa7113720bcd1c0a3effb37cb88e5ce83e64d1
Oliver Madge <oghm2-github@srcf.net>
2013-11-03 16:46:33 +0000

    Add square tool.

--- html/js/tools/square.js
+++ html/js/tools/square.js
@@ -1,10 +1,36 @@
 /*global $:false, jQuery:false */
 
 var square = {
+    start : undefined,
+    square : undefined,
     loaded : function() {},
     selected: function() {
             console.log("square!");
+        },
+    mousedown: function(event) {
+        // If we produced a drawnpath before, deselect it:
+        if (this.start) {
+            //complete a rectangle
+            var shape = new paper.Path.Rectangle(this.start, event.point);
+            shape.strokeColor = 'black';
+            this.start = undefined;
         }
+        else {
+            this.start = event.point;
+            this.square = new paper.Path.Rectangle(this.start, event.point);
+            this.square.strokeColor = 'black';
+        }
+
+        paper.view.draw();
+    },
+    mousemove: function(event) {
+        if (this.start) {
+            this.square.remove();
+            this.square = new paper.Path.Rectangle(this.start, event.point);
+            this.square.strokeColor = 'black';
+            paper.view.draw();
+        }
+    },
     };
 
 tools.square = square;
