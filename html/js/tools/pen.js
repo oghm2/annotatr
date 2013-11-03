645981d4c6b488c88c50e6462bd67f92da119910
Oliver Madge <oghm2-github@srcf.net>
2013-11-03 12:22:55 +0000

    Made toolbox tools modular

---
+++ html/js/tools/pen.js
@@ -0,0 +1,10 @@
+/*global $:false, jQuery:false */
+
+var pen = {
+    selected: function() {
+            console.log("pen!");
+        }
+    };
+
+tools.pen = pen;
+
---
+++ html/js/tools/square.js
@@ -0,0 +1,10 @@
+/*global $:false, jQuery:false */
+
+var square = {
+    selected: function() {
+            console.log("square!");
+        }
+    };
+
+tools.square = square;
+
