fef38795cee0d7b73ddd7c52512cb649d5e0dc43
Jeff Snyder <jeff-github@caffeinated.me.uk>
2013-11-03 13:57:19 +0000

    sticky selection

--- html/js/tools/select.js
+++ html/js/tools/select.js
@@ -5,12 +5,34 @@ var select = {
     selected: function() {
         console.log("select!");
     },
+    deselected: function() {
+        console.log("!select");
+    },
+    click: function() {
+        this.state.selected_item = this.state.highlighted_item;
+    },
     mousemove: function(event) {
-        paper.project.activeLayer.selected = false;
-        var hr = paper.project.hitTest(getMousePoint(canvas, event));
-        if (hr)
-            hr.item.selected = true;
-   },
+        var new_highlighted_item = undefined;
+        var hr = paper.project.hitTest(event.point);
+
+        if (hr) {
+            new_highlighted_item = hr.item;
+        } else {
+            new_highlighted_item = this.state.selected_item;
+        }
+
+        if (this.state.highlighted_item != new_highlighted_item) {
+            if (this.state.highlighted_item)
+                this.state.highlighted_item.selected = false;
+            this.state.highlighted_item = new_highlighted_item;
+            if (this.state.highlighted_item)
+                this.state.highlighted_item.selected = true;
+        }
+    },
+    state: {
+        selected_item: undefined,
+        highlighted_item: undefined,
+    },
 };
 
 tools.select = select;
